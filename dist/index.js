61674cd8b0ffd53d95b4a0608b240048654dee62
Wouter de Vos <wouter@codaisseur.com>
2017-06-09 15:16:19 +0200

    Add CORS headers

--- dist/index.js
+++ dist/index.js
@@ -4,16 +4,29 @@ const express = require("express");
 const twitter_1 = require("./twitter");
 const sentiment_1 = require("./sentiment");
 const PORT = process.env.PORT || 5000;
+const allowCrossDomain = function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+};
 const app = express();
 app
+    .use(allowCrossDomain)
     .get('/', (req, res) => {
     res.send({ message: 'hello, world!' });
 })
     .get('/search/:term', (req, res) => {
     const searchTerm = req.params.term;
+    res.header['Access-Control-Allow-Origin'] = '*';
+    res.header['Access-Control-Allow-Methods'] = 'GET,PUT,POST,DELETE';
+    res.header['Access-Control-Allow-Headers'] = 'Content-Type';
     console.log('Searching for', searchTerm);
     twitter_1.default(searchTerm)
         .then((results) => (sentiment_1.default(results.data.statuses)
+        .on('response', (response) => {
+        response.headers['Access-Control-Allow-Origin'] = '*';
+    })
         .pipe(res)));
 })
     .listen(PORT, () => console.log('Listening on port 5000'));
--- src/index.ts
+++ src/index.ts
@@ -5,19 +5,35 @@ import sentiment from './sentiment'
 
 const PORT = process.env.PORT || 5000
 
+const allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+  next();
+}
+
 const app = express()
 
 app
+  .use(allowCrossDomain)
   .get('/', (req, res) => {
     res.send({ message: 'hello, world!' })
   })
 	.get('/search/:term', (req, res) => {
 		const searchTerm = req.params.term
 
+    res.header['Access-Control-Allow-Origin'] = '*'
+    res.header['Access-Control-Allow-Methods'] = 'GET,PUT,POST,DELETE'
+    res.header['Access-Control-Allow-Headers'] = 'Content-Type'
+
     console.log('Searching for', searchTerm)
     search(searchTerm)
       .then((results) => (
         sentiment(results.data.statuses)
+          .on('response', (response) => {
+            response.headers['Access-Control-Allow-Origin'] = '*'
+          })
           .pipe(res)
       ))
 	})
