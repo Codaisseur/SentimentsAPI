90df3bdd3a1af7c1545ea8d55fcd27dc8adcc2c5
Wouter de Vos <wouter@codaisseur.com>
2017-06-09 14:09:43 +0200

    Make PORT an env var

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import * as express from 'express'
 import search from './twitter'
 import sentiment from './sentiment'
 
+const PORT = process.env.PORT || 5000
+
 const app = express()
 
 app
@@ -16,4 +18,4 @@ app
           .pipe(res)
       ))
 	})
-	.listen(5000, () => console.log('Listening on port 5000'))
+	.listen(PORT, () => console.log('Listening on port 5000'))
